4f66c753966800754231f3a533308cfcaabc7917
NadiaCorrea <ncormor536@gmail.com>
2022-11-17 21:36:41 +0100

    adding saleId

--- src/main/java/com/jacaranda/model/SaleId.java
+++ src/main/java/com/jacaranda/model/SaleId.java
@@ -1,31 +1,46 @@
 package com.jacaranda.model;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-public class SaleId {
-	private int eleId; //database ele_id
-	private int usId; //database us_id
+public class SaleId implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8238599676946189014L;
+	private int element; //database ele_id
+	private int user; //database us_id
 	private LocalDateTime salesDate;
 
 	public SaleId() {
 		
 	}
 	
-	public int getEleId() {
-		return eleId;
+
+	public int getElement() {
+		return element;
 	}
 
-	public void setEleId(int eleId) {
-		this.eleId = eleId;
+
+
+	public void setElement(int element) {
+		this.element = element;
 	}
 
-	public int getUsId() {
-		return usId;
+
+
+	public int getUser() {
+		return user;
 	}
 
-	public void setUsId(int usId) {
-		this.usId = usId;
+
+
+	public void setUser(int user) {
+		this.user = user;
 	}
+
+
 
 	public LocalDateTime getSalesDate() {
 		return salesDate;
@@ -39,4 +54,30 @@ public class SaleId {
 		this.salesDate = salesDate;
 	}
 
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(element, salesDate, user);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SaleId other = (SaleId) obj;
+		return element == other.element && Objects.equals(salesDate, other.salesDate) && user == other.user;
+	}
+
+
+	@Override
+	public String toString() {
+		return "SaleId [element=" + element + ", user=" + user + ", salesDate=" + salesDate + "]";
+	}
+
+	
 }
